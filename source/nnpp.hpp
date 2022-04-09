a460109e7a19c124cb9636fb3ca21ce144252213
Giannis Gkoulioumis <giannis.gkoulioumis@gmail.com>
2022-04-09 13:48:21 +0200

    Add some more functionality to stack vector

--- source/nnpp.hpp
+++ source/nnpp.hpp
@@ -47,8 +47,11 @@ public:
 	inline uint size() const { return m_size; }
 	inline auto begin() const { return m_array.begin(); }
 	inline auto end() const { return m_array.begin() + m_size; }
+	inline bool empty() const { return m_size == 0; }
+	inline void erase(uint index) { std::swap(m_array[index], m_array[--m_size]); }
 	inline T& operator[](uint pos) { m_size = std::max(m_size, pos + 1); return m_array[pos]; }
 	inline const T& operator[](uint pos) const { return m_array[pos]; }
+
 	inline StackVector& operator=(StackVector&& other) {
 		m_array = std::move(other.m_array);
 		m_size = std::move(other.m_size);
